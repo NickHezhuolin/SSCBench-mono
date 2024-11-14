e0cd33f68e3914c56d097e5d9238f44df675cb03
FIXED-TERM HE Zhuolin (CR/RIX3-AP) <hez4sgh@SGH-C-003YM.sgh.apac.bosch.com>
2024-11-14 16:14:00 +0800

    update mix_dataset_dm: add if-else for double triple dataset settings

--- method/MonoScene/monoscene/data/mix_dataset/mix_dataset_dm.py
+++ method/MonoScene/monoscene/data/mix_dataset/mix_dataset_dm.py
@@ -15,10 +15,13 @@ class MixDataModule(pl.LightningDataModule):
         kitti360_dataset_preprocess_root,
         nuscenes_dataset_root,
         nuscenes_dataset_preprocess_root,
+        waymo_dataset_root,
+        waymo_dataset_preprocess_root,
         project_scale=2,
         frustum_size=4,
         batch_size=4, 
         num_workers=6,
+        dataset="kitti_360_waymo",
     ):
 
         super().__init__()
@@ -26,10 +29,13 @@ class MixDataModule(pl.LightningDataModule):
         self.kitti360_dataset_preprocess_root = kitti360_dataset_preprocess_root
         self.nuscenes_dataset_root = nuscenes_dataset_root
         self.nuscenes_dataset_preprocess_root = nuscenes_dataset_preprocess_root
+        self.waymo_dataset_root = waymo_dataset_root
+        self.waymo_dataset_preprocess_root = waymo_dataset_preprocess_root
         self.project_scale = project_scale
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.frustum_size = frustum_size
+        self.dataset=dataset
 
     def setup(self, stage=None):
         kitti360_train_ds = Kitti360Dataset(
@@ -62,9 +68,22 @@ class MixDataModule(pl.LightningDataModule):
             color_jitter=(0.4, 0.4, 0.4),
         )
         
-        self.train_ds = ConcatDataset([kitti360_train_ds, nuscenes_train_ds])
-        self.train_ds = ConcatDataset([kitti360_train_ds, waymo_train_ds])
-        self.train_ds = ConcatDataset([nuscenes_train_ds, waymo_train_ds])
+        # double dataset training
+        if self.dataset == "kitti_360_nuscenes":
+            self.train_ds = ConcatDataset([kitti360_train_ds, nuscenes_train_ds])
+            print("Using kitti_360 and nuscenes datasets for training.")
+        elif self.dataset == "kitti_360_waymo":
+            self.train_ds = ConcatDataset([kitti360_train_ds, waymo_train_ds])
+            print("Using kitti_360 and waymo datasets for training.")
+        elif self.dataset == "nuscenes_waymo":
+            self.train_ds = ConcatDataset([nuscenes_train_ds, waymo_train_ds])
+            print("Using nuscenes and waymo datasets for training.")
+        elif self.dataset == "kitti_360_nuscenes_waymo":
+            # triple dataset training
+            self.train_ds = ConcatDataset([kitti360_train_ds, nuscenes_train_ds, waymo_train_ds])
+            print("Using kitti_360, nuscenes, and waymo datasets for training.")
+        else:
+            raise ValueError(f"Unknown dataset combination: {self.dataset}.")
 
         self.val_ds = Kitti360Dataset(
             split="val",
@@ -113,7 +132,7 @@ class MixDataModule(pl.LightningDataModule):
             shuffle=True,
             pin_memory=True,
             worker_init_fn=worker_init_fn,
-            collate_fn=collate_fn,
+            collate_fn=collate_fn_mix,
         )
 
     def val_dataloader(self):
